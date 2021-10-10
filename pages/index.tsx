9454dd5dd9ba34a53f96dde7490d14f120d3cdb7
Keiner Peñaranda <keinerjpp@gmail.com>
2021-10-10 10:14:52 -0500

    proyecto terminado

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
+import Layout from '@components/Layout/Layout'
+import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
+import ProductList from '@components/ProductList/ProductList'
+
+export const getStaticProps = async () =>{
+    const response = await fetch('https://platzi-avo.vercel.app/api/avo')
+    const {data: productList}: TAPIAvoResponse = await response.json()
+
+  return {
+    props:{
+      productList,
+    }
+  }
+}
+
+const HomePage = ({productList}: {productList: TProduct[] }) => {
+
+  return (
+    <Layout>
+      <KawaiiHeader />
+      <ProductList products={productList} />
+    </Layout>
+  )
+}
+
+export default HomePage
---
+++ pages/product/[id].tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+
+import Layout from '@components/Layout/Layout'
+import ProductSummary from '@components/ProductSummary/ProductSummary'
+
+export const getStaticPaths = async () =>{
+  const response = await fetch('https://platzi-avo.vercel.app/api/avo')
+  const {data: productList}: TAPIAvoResponse = await response.json()
+
+  const paths = productList.map(({id}) =>({
+    params: {
+      id
+    }
+  }))
+
+   return{
+     paths,
+     fallback: false
+   }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) =>{
+  const id = params?.id as string
+  const response = await fetch(`https://platzi-avo.vercel.app/api/avo/${id}`)
+  const product: TProduct = await response.json()
+
+return {
+  props:{
+    product,
+  }
+}
+}
+
+const ProductPage = ({product}: {product: TProduct}) => {
+
+  return (
+    <Layout>
+      {product == null ? null : <ProductSummary product={product} />}
+    </Layout>
+  )
+}
+
+export default ProductPage
